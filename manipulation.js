31df00402d7142596828b20290789ef9f7a01d66
Danilo <danilo.sl09@aluno.ifsc.edu.br>
2023-04-17 20:39:09 -0300

    Quarto commit

--- manipulation.js
+++ manipulation.js
@@ -14,5 +14,44 @@ manipulation.getAllProducts = (req, res) => {
     res.status(200).send(listProducts)
 };
 
+manipulation.getProductById = (req, res) => {
+    item = listProducts.find(i => i.id == req.params.id)
+
+    if(item){
+        res.status(200).send(item)
+    }else{
+        res.status(404).send("Product not find")
+    }
+};
+
+manipulation.createProductById = (req,res)=>{
+    const newProduct = req.body
+    newProduct.id = indice + 1
+    listProducts.push(newProduct)
+    res.status(200).redirect("/indice")
+};
+
+manipulation.updateProductById = (req,res)=>{
+    productIndice = listProducts.findIndex(p => p.id == req.params.id)
+    console.log(productIndice)
+    if(productIndice >= 0){
+      const pessoaAtualizada = req.body;
+      listProducts[productIndice] = pessoaAtualizada;
+      res.status(200).send("OK")
+    }else{
+      res.status(404).send('Error 404')
+    }
+};
+
+manipulation.deleteProductById = (req,res)=>{
+    const pessoaIndice = listProducts.findIndex(p => p.id == req.params.id);
+    if (pessoaIndice >= 0) {
+        listProducts.splice(pessoaIndice, 1);
+      res.status(200).send("OK")
+    }else{
+      res.status(404).sendFile(path.resolve(__dirname+"/../views/notfound.html"))
+    }
+  }
+
 
 module.exports = manipulation
--- routes.js
+++ routes.js
@@ -4,5 +4,11 @@ const manipulation = require("./manipulation");
 const routes = express.Router();
 
 routes.get("/indice", manipulation.getAllProducts);
+routes.get("/indice/:id", manipulation.getProductById);
+routes.post("/indice/:id", manipulation.createProductById);
+routes.put("/indice/:id", manipulation.updateProductById);
+routes.delete("/indice/:id", manipulation.deleteProductById);
+
+
 
 module.exports = routes;
